4bcea593c748451306da29ecd6a8ff9914d30111
Wolf McNally <wolf@wolfmcnally.com>
2022-07-24 15:12:22 -0700

    Refactoring.

--- Sources/BCSecureComponents/ECKey.swift
+++ Sources/BCSecureComponents/ECKey.swift
@@ -143,16 +143,6 @@ public struct ECPublicKey: ECPublicKeyProtocol, Hashable {
     public var uncompressed: ECUncompressedPublicKey {
         return ECUncompressedPublicKey(Wally.ecPublicKeyDecompress(data: data))!
     }
-    
-//    public func address(version: UInt8) -> String {
-//        var hash = hash160
-//        hash.insert(version, at: 0)
-//        return hash.base58(isCheck: true)
-//    }
-    
-//    public func address(useInfo: UseInfo, isSH: Bool) -> String {
-//        address(version: isSH ? useInfo.versionSH : useInfo.versionPKH)
-//    }
 
     public var `public`: ECPublicKey {
         self
