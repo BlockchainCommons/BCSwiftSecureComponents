906c6462ea6df0aa33e6c07778178c295622f2f0
Wolf McNally <wolf@wolfmcnally.com>
2022-11-07 14:18:58 -0800

    Cleanup.

--- Sources/BCSecureComponents/Envelope/EnvelopeTreeFormat.swift
+++ Sources/BCSecureComponents/Envelope/EnvelopeTreeFormat.swift
@@ -3,11 +3,30 @@ import Graph
 
 extension Envelope {
     public var treeFormat: String {
-        graph(data: ()).treeFormat
+        var elements: [TreeElement] = []
+        addEnvelope(self, level: 0, incomingEdge: .none, result: &elements)
+        return elements.map { $0.string }.joined(separator: "\n")
+    }
+
+    fileprivate func addEnvelope(_ envelope: Envelope, level: Int, incomingEdge: EdgeType, result: inout [TreeElement]) {
+        result.append(TreeElement(level: level, envelope: envelope, incomingEdge: incomingEdge))
+        let level = level + 1
+        switch envelope {
+        case .node(let subject, let assertions, _):
+            addEnvelope(subject, level: level, incomingEdge: .subject, result: &result)
+            for assertion in assertions {
+                addEnvelope(assertion, level: level, incomingEdge: .assertion, result: &result)
+            }
+        case .assertion(let assertion):
+            addEnvelope(assertion.predicate, level: level, incomingEdge: .predicate, result: &result)
+            addEnvelope(assertion.object, level: level, incomingEdge: .object, result: &result)
+        case .wrapped(let envelope, _):
+            addEnvelope(envelope, level: level, incomingEdge: .wrapped, result: &result)
+        default:
+            break
+        }
     }
 }
-
-typealias TreeEnvelopeGraph = Graph<Int, Int, Envelope, EnvelopeEdgeData, Void>
 
 fileprivate struct TreeElement {
     let level: Int
@@ -30,33 +49,5 @@ fileprivate struct TreeElement {
             .joined(separator: " ")
         let indent = String(repeating: " ", count: level * 4)
         return indent + line
-    }
-}
-
-fileprivate extension TreeEnvelopeGraph {
-    var treeFormat: String {
-        let envelope = try! nodeData(nodes.first!)
-        var elements: [TreeElement] = []
-        addEnvelope(envelope, level: 0, incomingEdge: .none, result: &elements)
-        return elements.map { $0.string }.joined(separator: "\n")
-    }
-    
-    func addEnvelope(_ envelope: Envelope, level: Int, incomingEdge: EdgeType, result: inout [TreeElement]) {
-        result.append(TreeElement(level: level, envelope: envelope, incomingEdge: incomingEdge))
-        let level = level + 1
-        switch envelope {
-        case .node(let subject, let assertions, _):
-            addEnvelope(subject, level: level, incomingEdge: .subject, result: &result)
-            for assertion in assertions {
-                addEnvelope(assertion, level: level, incomingEdge: .assertion, result: &result)
-            }
-        case .assertion(let assertion):
-            addEnvelope(assertion.predicate, level: level, incomingEdge: .predicate, result: &result)
-            addEnvelope(assertion.object, level: level, incomingEdge: .object, result: &result)
-        case .wrapped(let envelope, _):
-            addEnvelope(envelope, level: level, incomingEdge: .wrapped, result: &result)
-        default:
-            break
-        }
     }
 }
