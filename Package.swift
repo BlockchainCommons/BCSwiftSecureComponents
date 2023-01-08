f66ab3d7f299c8567dcaf384bacf7793acbb1c63
Wolf McNally <wolf@wolfmcnally.com>
2023-01-07 16:26:11 -0800

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -5,8 +5,10 @@ import PackageDescription
 let package = Package(
     name: "SecureComponents",
     platforms: [
+        .macOS(.v12),
         .iOS(.v15),
-        .macOS(.v13)
+        .tvOS(.v15),
+        .watchOS(.v8)
     ],
     products: [
         .library(
@@ -14,8 +16,8 @@ let package = Package(
             targets: ["SecureComponents", "BCWally", "SSKR"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/WolfMcNally/WolfBase", from: "4.0.0"),
-        .package(url: "https://github.com/BlockchainCommons/URKit.git", from: "7.0.0"),
+        .package(url: "https://github.com/WolfMcNally/WolfBase", from: "5.0.0"),
+        .package(url: "https://github.com/BlockchainCommons/URKit.git", from: "8.0.0"),
         .package(url: "https://github.com/BlockchainCommons/blake3-swift.git", from: "0.1.2"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.1"),
         .package(url: "https://github.com/BlockchainCommons/secp256k1-zkp.swift.git", from: "0.5.0"),
