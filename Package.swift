81edaaf4feccbda7b1987583bfa5e59e2460b049
Wolf McNally <wolf@wolfmcnally.com>
2023-09-06 01:48:45 -0700

    Aligned to dependencies.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/WolfMcNally/WolfBase", from: "5.0.0"),
-        .package(url: "https://github.com/BlockchainCommons/URKit.git", from: "11.3.1"),
+        .package(url: "https://github.com/BlockchainCommons/URKit.git", from: "11.3.2"),
         .package(url: "https://github.com/BlockchainCommons/BCSwiftCrypto", from: "0.7.1"),
     ],
     targets: [
